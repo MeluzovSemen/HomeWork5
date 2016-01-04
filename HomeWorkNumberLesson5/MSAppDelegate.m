db3f178436dc3b95451d96dda274c79576907f05
Meluzov <semen.meluzov@mail.ru>
2016-01-04 18:57:12 +0500

    NewBranche

--- HomeWorkNumberLesson5/MSAppDelegate.m
+++ HomeWorkNumberLesson5/MSAppDelegate.m
@@ -22,6 +22,9 @@
 {
     
     
+    //==== new branches =====//
+    
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
